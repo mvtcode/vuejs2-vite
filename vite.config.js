a240573b32ce275a4024fb21aa0012392306ba66
Tanmv <tanmv@mpos.vn>
2024-06-20 09:37:50 +0700

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-const { createVuePlugin } = require('vite-plugin-vue2');
+import { fileURLToPath, URL } from 'url';
+import { createVuePlugin } from 'vite-plugin-vue2';
 
 module.exports = {
   plugins: [createVuePlugin()],
+  build: {
+    sourcemap: false
+  },
+  css: {
+    devSourcemap: false
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 };
